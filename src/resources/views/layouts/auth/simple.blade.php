67425560e4ce30583fbb8cf11cfccb6a74473526
devl333 <superphs333@naver.com>
2026-04-23 10:08:05 +0900

    로고 크기 조정

--- src/resources/views/layouts/auth/simple.blade.php
+++ src/resources/views/layouts/auth/simple.blade.php
@@ -7,8 +7,8 @@
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="mb-2 flex size-16 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-16" />
+                    <span class="mb-3 flex size-24 items-center justify-center rounded-md">
+                        <x-app-logo-icon class="size-24" />
                     </span>
                     <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
                 </a>
