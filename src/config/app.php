ef42b42fbc04b2fa03e349c99c289ed558f3ef1a
devl333 <superphs333@naver.com>
2026-06-19 15:54:51 +0900

    마이페이지 다국어 - app 설정

--- src/config/app.php
+++ src/config/app.php
@@ -78,11 +78,16 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'ko'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+
+    'supported_locales' => [
+        'ko' => 'Korean',
+        'en' => 'English',
+    ],
 
     /*
     |--------------------------------------------------------------------------
